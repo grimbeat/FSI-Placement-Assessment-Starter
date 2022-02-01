1f32ab586a805bcb70900b74887348142c45a8e8
grimbeat <soobin131@gmail.com>
2022-01-31 21:06:39 -0500

    almost done

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,79 @@
 // HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
-// First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
 
-// We'll use these variables to track the counts of each cookie type
+let yourName = "Subin Julie Hwang" 
+
+// Gingerbread
+function updateQtygb(displayQtygb) {
+    let qtygb = document.querySelector("#qty-gb")
+    qtygb.innerHTML = displayQtygb
+}
+//Chocolate Chip
+function updateQtycc(displayQtycc) {
+    let qtycc = document.querySelector("#qty-cc")
+    qtycc.innerHTML = displayQtycc
+}
+//Sugar Sprinkle
+function updateQtysugar(displayQtysugar) {
+    let qtysugar = document.querySelector("#qty-sugar")
+    qtysugar.innerHTML = displayQtysugar
+}
+
 let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
+let total = 0
 
 // selecting the element with an id of credit
 const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
+
+// Gingerbread
 const gbPlusBtn = document.querySelector('#add-gb')
+const gbMinusBtn = document.querySelector('#minus-gb')
+// Chocolate Chip
+const ccPlusBtn = document.querySelector('#add-cc')
+const ccMinusBtn = document.querySelector('#minus-cc')
+// Sugar Sprinkle
+const sugarPlusBtn = document.querySelector('#add-sugar')
+const sugarMinusBtn = document.querySelector('#minus-sugar')
 
 // Code to update name display
 credit.textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
+//Ginger Bread Btn
 gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
+    qtygb = gb++
+    updateQtygb(qtygb)
+})
+gbMinusBtn.addEventListener('click', function() {
+    if (gb >= 0) {
+        qtygb = gb--
+    }
+    updateQtygb(qtygb)
+})
 
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+//Chocolate Chip Btn
+ccPlusBtn.addEventListener('click', function() {
+    qtycc = cc++
+    updateQtycc(qtycc)
+})
+ccMinusBtn.addEventListener('click', function() {
+    if (cc >= 0) {
+        qtycc = cc--
+    }
+    updateQtycc(qtycc)
+})
+
+//Sugar Sprinkle Btn
+ccPlusBtn.addEventListener('click', function() {
+    qtycc = cc++
+    updateQtycc(qtycc)
+})
+ccMinusBtn.addEventListener('click', function() {
+    if (cc >= 0) {
+        qtycc = cc--
+    }
+    updateQtycc(qtycc)
 })
 
 // TODO: Hook up event listeners for the rest of the buttons
